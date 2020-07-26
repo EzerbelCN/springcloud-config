757bcd00f57a11f59cf643734412bb07bc941477
EzerbelCN <ezerbelcn@gmail.com>
2020-07-26 17:55:51 +0800

    ignore test.txt

--- src/main/java/com/ezerbel/springcloud/MainAppConfigCenter3344.java
+++ src/main/java/com/ezerbel/springcloud/MainAppConfigCenter3344.java
@@ -2,6 +2,7 @@ package com.ezerbel.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -10,9 +11,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @Date: 2020/7/24 20:41
  */
 @SpringBootApplication
-@EnableEurekaClient
 @EnableConfigServer
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class MainAppConfigCenter3344 {
+    //test1
     public static void main(String[] args){
         SpringApplication.run(MainAppConfigCenter3344.class,args);
     }
